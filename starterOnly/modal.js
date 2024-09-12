182cc6c82ce64c47523af173d50cda7387904fe0
aissa henni <henni.issa@gmail.com>
2024-09-12 10:43:36 +0200

    close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,72 +1,36 @@
-const modalBackground = document.querySelector(".bground");
-const closeModalButton = document.querySelector(".close");
-const conditionsAcceptedCheckbox = document.querySelector("#checkbox1");
-const submitButton = document.querySelector(".btn-submit");
-const firstNameInput = document.querySelector("#first");
-const lastNameInput = document.querySelector("#last");
-const emailInput = document.querySelector("#email");
-const birthdateInput = document.querySelector("#birthdate");
-const quantityInput = document.querySelector("#quantity");
-const modalButtons = document.querySelectorAll(".modal-btn");
-const formInputs = document.querySelectorAll(".formData");
-const location1 = document.querySelector("#location1");
-const form = document.querySelector("form");
 
-if (form == null) throw new Error("No form found");
-if (conditionsAcceptedCheckbox == null) throw new Error("No checkbox found");
-if (location1 == null) throw new Error("No location1 found");
-if (lastNameInput == null) throw new Error("No lastNameInput inputs found");
-if (emailInput == null) throw new Error("No emailInput inputs found");
-if (birthdateInput == null) throw new Error("No birthdateInput inputs found");
-if (firstNameInput == null) throw new Error("No firstNameInput inputs found");
-if (closeModalButton == null) throw new Error("No close button found");
-
-firstNameInput.addEventListener("invalid", flashErrorMessage);
-lastNameInput.addEventListener("invalid", flashErrorMessage);
-emailInput.addEventListener("invalid", flashErrorMessage);
-birthdateInput.addEventListener("invalid", flashErrorMessage);
-location1.addEventListener("invalid", flashErrorMessage);
-conditionsAcceptedCheckbox.addEventListener("invalid", flashErrorMessage);
-
-form.addEventListener("submit", handleSubmit);
-
-modalButtons.forEach((btn) => btn.addEventListener("click", launchModal));
-closeModalButton.addEventListener("click", closeModal);
-
-function handleSubmit() {
-  if (form == null) throw new Error("No form found");
-  if (form.checkValidity()) {
-    closeModal();
-    alert("Merci ! Votre réservation a été reçue.");
+// Toggles responsive mode for the navigation menu 
+function editNav() {
+  const x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
 }
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelector(".close");
 
-function flashErrorMessage(event) {
-  const target = event.target;
-  const parent = target.parentElement;
-  parent.setAttribute("data-error-visible", "true");
-  setTimeout(removeErrorMessages, 10000);
-}
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-function removeErrorMessages() {
-  const errorMessages = document.querySelectorAll("[data-error]");
-  errorMessages.forEach((errorMessage) => {
-    errorMessage.removeAttribute("data-error-visible");
-  });
-}
 
-function closeModal() {
-  if (modalBackground == null) throw new Error("No modal background found");
-  modalBackground.classList.remove("active");
-}
-
+// launch modal form
 function launchModal() {
-  if (modalBackground == null) throw new Error("No modal background found");
-  modalBackground.classList.add("active");
+  modalbg.style.display = "block";
 }
 
-function editNav() {
-  const topNavbar = document.getElementById("myTopnav");
-  if (topNavbar == null) throw new Error("No topnav found");
-  topNavbar.classList.toggle("responsive");
+// Add event listener to close modal[Aissa]
+closeModalBtn.addEventListener("click", closeModal);
+
+
+
+// Close modal function [Aissa]
+function closeModal() {
+modalbg.style.display = "none";
 }
+
+
